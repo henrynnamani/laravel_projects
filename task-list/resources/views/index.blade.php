b98c8773216ffe3eb601e22d21a4332db9d058e2
gr4yx01 <hoyx0101@gmail.com>
2025-08-11 00:09:32 +0100

    feat:implement edit, paginate task

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -5,9 +5,15 @@
 @section('content')
     @forelse($tasks as $task)
         <p>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
         </p>
     @empty
         <div>There're no tasks</div>
     @endforelse
+
+    @if($tasks->count())
+        <div>
+            {{ $tasks->links() }}
+        </div>
+    @endif
 @endsection
--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -4,11 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @yield('styles')
     <title>Task List Application</title>
 </head>
 
 <body>
     <h1>@yield('title')</h1>
+    @if (session()->has('success'))
+        <p>{{ session('success') }}</p>
+    @endif
     <div>
         @yield('content')
     </div>
